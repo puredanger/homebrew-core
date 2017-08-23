b597d4e3de73ce16f4e6387f9c920dcdabfdba3c
puredanger <alex.miller@cognitect.com>
2017-08-23 11:36:09 -0500

    clojure 1.8.0.119 (new formula)

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -1,11 +1,14 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
+  version "1.8.0.119"
+  url "https://download.clojure.org/install/brew/install-clj-1.8.0.119.tar.gz"
+  sha256 "7e272d3ae13d97c8fb6848d6851112e88995958391f40a1787bd82b6222d779e"
 
   devel do
-    version "1.9.0-alpha18.116"
-    url "https://download.clojure.org/install/brew/install-clj-1.9.0-alpha18.116.tar.gz"
-    sha256 "9a11824797190fb6603c8022dbc6398d7a1ae91fb2da56c0587d39f83a40a060"
+    version "1.9.0-alpha18.118"
+    url "https://download.clojure.org/install/brew/install-clj-1.9.0-alpha18.118.tar.gz"
+    sha256 "93d59d7283a488b1d2fa21038519033543ca36e2f9ac50ccacf2a8801113cba7"
   end
 
   bottle :unneeded
@@ -14,7 +17,7 @@ class Clojure < Formula
   depends_on "rlwrap"
 
   def install
-    prefix.install "install-clj-1.9.0-alpha18.116.jar"
+    prefix.install "install-clj-1.9.0-alpha18.118.jar"
     prefix.install "clj.props"
     inreplace "install-clj", /PREFIX/, prefix
     bin.install "install-clj"
