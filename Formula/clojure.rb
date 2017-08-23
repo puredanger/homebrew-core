1bf4ed9e143f4c3dd19d6c8b87f947cb07c488df
puredanger <alex.miller@cognitect.com>
2017-08-23 11:43:19 -0500

    clojure 1.8.0.119 (new formula)
    Fix audit warnings

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -1,14 +1,13 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  version "1.8.0.119"
   url "https://download.clojure.org/install/brew/install-clj-1.8.0.119.tar.gz"
   sha256 "7e272d3ae13d97c8fb6848d6851112e88995958391f40a1787bd82b6222d779e"
 
   devel do
-    version "1.9.0-alpha18.118"
-    url "https://download.clojure.org/install/brew/install-clj-1.9.0-alpha18.118.tar.gz"
-    sha256 "93d59d7283a488b1d2fa21038519033543ca36e2f9ac50ccacf2a8801113cba7"
+    url "https://download.clojure.org/install/brew/install-clj-1.9.0-alpha18.120.tar.gz"
+    sha256 "92946dd137c394d954b47513aa87a39578e4a2c893ff3501710072f170b07aca"
+    version "1.9.0-alpha18.120"
   end
 
   bottle :unneeded
@@ -17,7 +16,7 @@ class Clojure < Formula
   depends_on "rlwrap"
 
   def install
-    prefix.install "install-clj-1.9.0-alpha18.118.jar"
+    prefix.install "install-clj-1.9.0-alpha18.120.jar"
     prefix.install "clj.props"
     inreplace "install-clj", /PREFIX/, prefix
     bin.install "install-clj"
