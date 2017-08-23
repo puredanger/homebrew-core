ae6c116ab78d1ef0856588194f6b092cb6254bd9
puredanger <alex.miller@cognitect.com>
2017-08-23 12:09:08 -0500

    remove version-dependent reference in install

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -5,9 +5,9 @@ class Clojure < Formula
   sha256 "7e272d3ae13d97c8fb6848d6851112e88995958391f40a1787bd82b6222d779e"
 
   devel do
-    url "https://download.clojure.org/install/brew/install-clj-1.9.0-alpha18.120.tar.gz"
-    sha256 "92946dd137c394d954b47513aa87a39578e4a2c893ff3501710072f170b07aca"
-    version "1.9.0-alpha18.120"
+    url "https://download.clojure.org/install/brew/install-clj-1.9.0-alpha18.125.tar.gz"
+    sha256 "393c2f6025553376aebceaf359af08a9152edb9854ccc099460b06cf8dac1c5f"
+    version "1.9.0-alpha18.125"
   end
 
   bottle :unneeded
@@ -16,7 +16,7 @@ class Clojure < Formula
   depends_on "rlwrap"
 
   def install
-    prefix.install "install-clj-1.9.0-alpha18.120.jar"
+    prefix.install Dir["*.jar"]
     prefix.install "clj.props"
     inreplace "install-clj", /PREFIX/, prefix
     bin.install "install-clj"
